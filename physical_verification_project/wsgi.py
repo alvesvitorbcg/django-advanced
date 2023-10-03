a2145ad38b35c7b206f510caa4fbd651b92f92c2
Vítor Alves <alves.vitor@bcg.com>
2023-10-03 14:09:53 -0300

    recreating and configuring rest framework and swagger

--- physical_verification_project/wsgi.py
+++ physical_verification_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for physical_verification_system project.
+WSGI config for physical_verification_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'physical_verification_system.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'physical_verification_project.settings')
 
 application = get_wsgi_application()
