9a3170a249c7bb3807bb78d47fe6ab0659ca216e
Vítor Alves <alves.vitor@bcg.com>
2023-10-03 14:23:44 -0300

    configuring swagger

--- physical_verification_project/urls.py
+++ physical_verification_project/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from django.urls import include, path
 from rest_framework import routers
 from physical_verification_project.api import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -29,5 +30,11 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+      # YOUR PATTERNS
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
+
