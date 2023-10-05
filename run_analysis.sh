2ee87fe7c4d8e0bc448096dbffd6adcffb027a71
Vítor Alves <alves.vitor@bcg.com>
2023-10-05 13:48:46 -0300

    adjusting sonar exclusions

--- run_analysis.sh
+++ run_analysis.sh
@@ -3,7 +3,7 @@ coverage run ./manage.py test src && coverage report -m
 $PATH_TO_SONAR/sonar-scanner \
 -Dsonar.projectKey=physical-verification-system \
 -Dsonar.sources=src/ \
--Dsonar.exclusions=*/migrations/* \
--Dsonar.coverage.exclusions=**/tests/**,test_*.py,manage.py,physical_verification_project/* \
+-Dsonar.exclusions=**/migrations/** \
+-Dsonar.coverage.exclusions=**/tests/**,test_*.py,manage.py,**/physical_verification_project/** \
 -Dsonar.login=sqp_fa234517e3b80e943a233261e2e0ab2d367ab7df \
 -Dsonar.host.url=http://localhost:9000 \
