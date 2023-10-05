f5dc70f7e96dc4e7dd4c6b22bc48647a801fdcf4
Vítor Alves <alves.vitor@bcg.com>
2023-10-05 14:01:27 -0300

    removing unused code and adjusting sonar coverage report path

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,9 +1,10 @@
 PATH_TO_SONAR=$1
-coverage run ./manage.py test src && coverage report -m
+coverage run ./manage.py test src && coverage report -m && coverage xml
 $PATH_TO_SONAR/sonar-scanner \
 -Dsonar.projectKey=physical-verification-system \
 -Dsonar.sources=src/ \
 -Dsonar.exclusions=**/migrations/** \
--Dsonar.coverage.exclusions=**/tests/**,test_*.py,manage.py,**/physical_verification_project/** \
+-Dsonar.coverage.exclusions=**/tests/**,**/test*.py,manage.py,**/physical_verification_project/**,**/apps.py, \
+-Dsonar.python.coverage.reportPaths=coverage.xml \
 -Dsonar.login=sqp_fa234517e3b80e943a233261e2e0ab2d367ab7df \
 -Dsonar.host.url=http://localhost:9000 \
