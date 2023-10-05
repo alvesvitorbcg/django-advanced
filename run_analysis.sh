f17111b4d57e30b0f796ab732a21a15bdc399ece
Vítor Alves <alves.vitor@bcg.com>
2023-10-05 13:41:23 -0300

    setting sonar sources config to src/

--- run_analysis.sh
+++ run_analysis.sh
@@ -2,7 +2,7 @@ PATH_TO_SONAR=$1
 coverage run ./manage.py test && coverage report -m
 $PATH_TO_SONAR/sonar-scanner \
 -Dsonar.projectKey=physical-verification-system \
--Dsonar.sources=api,core,customer,loan_application,employee,physical_verification_project, \
+-Dsonar.sources=src/ \
 -Dsonar.exclusions=*/migrations/* \
 -Dsonar.coverage.exclusions=**/tests/**,test_*.py,manage.py,physical_verification_project/* \
 -Dsonar.login=sqp_fa234517e3b80e943a233261e2e0ab2d367ab7df \
