2c71db2cdedf75be4c8d1149902a0940bf11b28e
Vítor Alves <alves.vitor@bcg.com>
2023-10-05 21:36:36 -0300

    prevent verification document from being created for a loan application that is not in verification status assigned

--- src/verification_document/views.py
+++ src/verification_document/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, permissions
 from api import serializers
 from verification_document.models import VerificationDocument
+from loan_application.models import VerificationStatus, LoanApplication
+from rest_framework.serializers import ValidationError
 
 
 class VerificationDocumentViewSet(viewsets.ModelViewSet):
@@ -10,3 +12,12 @@ class VerificationDocumentViewSet(viewsets.ModelViewSet):
     queryset = VerificationDocument.objects.all()
     serializer_class = serializers.VerificationDocumentSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        loan_application = serializer.validated_data['loan_application']
+        if loan_application.verification_status != VerificationStatus.ASSIGNED.value:
+            raise ValidationError(
+                {"detail": "Verification status must be 'Assigned' before a verification document can be created."},
+            )
+        else:
+            serializer.save()
