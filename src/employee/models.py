9dc64a474c7e28bb3f41dd8f5a83eaf4a535847e
Vítor Alves <alves.vitor@bcg.com>
2023-10-05 21:21:55 -0300

    creating a user for each employee created

--- src/employee/models.py
+++ src/employee/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from core.models import BaseModel, Person
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Role(BaseModel):
@@ -11,3 +15,18 @@ class Role(BaseModel):
 
 class Employee(Person):
     role = models.ForeignKey(Role, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        'auth.User', on_delete=models.CASCADE, null=True, blank=True)
+
+
+@receiver(post_save, sender=Employee)
+def create_user_for_employee(sender, instance, created, **kwargs):
+    if created:
+        username = f"{instance.last_name[0]}{instance.first_name}{instance.id}"
+        password = "changeme"
+        email = f"{username}@nbfc.com"
+        user = User.objects.create_user(
+            username=username, password=password, email=email)
+        instance.user = user
+        instance.save()
+        print(f"Created user {username} with password {password}")
