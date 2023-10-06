6a5d63cfe07b74f3381dc1ea2c335fc4bd5cf3f4
Vítor Alves <alves.vitor@bcg.com>
2023-10-06 10:41:00 -0300

    permission class for verification document

--- src/verification_document/views.py
+++ src/verification_document/views.py
@@ -3,6 +3,25 @@ from api import serializers
 from verification_document.models import VerificationDocument
 from loan_application.models import VerificationStatus, LoanApplication
 from rest_framework.serializers import ValidationError
+from rest_framework.permissions import BasePermission
+from employee.models import Roles, Employee
+
+
+class IsUserVerifierOrReviewer(BasePermission):
+    """
+    Allows access only for employees with Verifier role.
+    """
+
+    def has_permission(self, request, view):
+        user = request.user
+        if user.is_superuser:
+            return True
+        employee = Employee.objects.filter(user=user).first()
+        if employee is None:
+            return False
+        if employee.role.role_type == Roles.VERIFIER.name or employee.role.role_type == Roles.REVIEWER.name:
+            return True
+        return False
 
 
 class VerificationDocumentViewSet(viewsets.ModelViewSet):
@@ -11,7 +30,8 @@ class VerificationDocumentViewSet(viewsets.ModelViewSet):
     """
     queryset = VerificationDocument.objects.all()
     serializer_class = serializers.VerificationDocumentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [
+        permissions.IsAuthenticated, IsUserVerifierOrReviewer]
 
     def perform_create(self, serializer):
         loan_application = serializer.validated_data['loan_application']
