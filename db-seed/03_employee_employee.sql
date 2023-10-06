108fd276e07890e8529df15b4cd649e4c923c897
Vítor Alves <alves.vitor@bcg.com>
2023-10-05 21:57:09 -0300

    assigning users to employees

--- db-seed/03_employee_employee.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO public.employee_employee (date_created,date_updated,first_name,middle_name,last_name,mobile,role_id) VALUES
-	 (NOW(),NOW(),'Marta','M','M','31996145581',1),
-	 (NOW(),NOW(),'Vitor','V','V','31996145581',2),
-	 (NOW(),NOW(),'Rakshara','R','R','31996145581',3);
