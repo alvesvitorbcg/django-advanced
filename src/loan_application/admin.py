e87996b59f99530d24320067fd7029c070a9611c
Vítor Alves <alves.vitor@bcg.com>
2023-10-09 21:29:07 -0300

    saving a loan application history record using post_save signal

--- src/loan_application/admin.py
+++ src/loan_application/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from loan_application.models import LoanApplication
+from loan_application.models import LoanApplication, LoanApplicationHistory
 
 admin.site.register(LoanApplication)
+admin.site.register(LoanApplicationHistory)
--- src/loan_application/models.py
+++ src/loan_application/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from core.models import BaseModel
 from customer.models import Customer
 from employee.models import Employee
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_save
 
 
 class Status(Enum):
@@ -48,9 +50,32 @@ class LoanApplication(BaseModel):
         return self.verification_status is VerificationStatus.VERIFIED.value
 
     def __str__(self) -> str:
-        return f'{self.id}_{self.customer}_{self.date_created}'
+        return f'{self.id}_{self.customer}'
 
 
-class LoanApplicationHistory(LoanApplication):
+class LoanApplicationHistory(BaseModel):
+    status = models.IntegerField(default=Status.NEW.value)
+    verification_status = models.IntegerField(
+        default=VerificationStatus.PENDING.value)
+    reviewer = models.ForeignKey(
+        Employee, on_delete=models.SET_NULL, null=True, blank=True, related_name='reviewer_history')
+    manager = models.ForeignKey(
+        Employee, on_delete=models.SET_NULL, null=True, blank=True, related_name='manager_history')
+    loan_amount = models.IntegerField()
+    customer = models.ForeignKey(
+        Customer, on_delete=models.SET_NULL, null=True, blank=True, related_name='customer_history')
+    verifier = models.ForeignKey(
+        Employee, on_delete=models.SET_NULL, null=True, blank=True, related_name='verifier_history')
     loan_application = models.ForeignKey(
         LoanApplication, on_delete=models.CASCADE, null=True, blank=True, related_name='loan_application_history')
+
+    def __str__(self) -> str:
+        return f'{self.id}_LA:{self.loan_application.id}_{self.customer}'
+
+
+@receiver(post_save, sender=LoanApplication)
+def create_loan_application_history(sender, instance, **kwargs):
+    history_instance = LoanApplicationHistory(loan_application=instance)
+    history_instance.__dict__.update(instance.__dict__)
+    history_instance.id = None
+    history_instance.save()
